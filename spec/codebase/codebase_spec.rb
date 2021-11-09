ece0c064d824aa98eb9589fd826787aadba2bd60
Su Ho <su.v@nimblehq.co>
2021-11-09 13:03:08 +0700

    [#1] Remove phraseapp

--- spec/codebase/codebase_spec.rb
+++ spec/codebase/codebase_spec.rb
@@ -34,23 +34,6 @@ describe 'Codebase', codebase: true do
     expect(find_results).to be_empty
   end
 
-  # rubocop:disable RSpec/ExampleLength
-  it 'does not offend PhraseApp Pull configuration' do
-    locale_tags = Dir[File.expand_path(Rails.root.join('config', 'locales', '*.yml'))]
-                  .map { |path| path.split('/').last } # ["campaign.en.yml", "article.en.yml", "campaign.th.yml"]
-                  .map { |path| path.split('.').first } # ["campaign", "article", "campaign"]
-                  .uniq # ["campaign", "article"]
-                  .sort # ["article", "campaign"]
-    phrase_app_pull_tags = YAML
-                           .load_file(Rails.root.join('.phraseapp.yml'))
-                           .dig('phraseapp', 'pull', 'targets')
-                           .map { |target| target.dig('params', 'tags') }
-                           .sort
-
-    expect(phrase_app_pull_tags).to eq locale_tags
-  end
-  # rubocop:enable RSpec/ExampleLength
-
   it 'does not offend scss-lint' do
     expect(`scss-lint`).to be_empty
   end
