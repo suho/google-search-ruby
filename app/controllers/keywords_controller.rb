4e6ebb92d6446e0c3fc79c49ec86852becef4387
Su Ho <su.v@nimblehq.co>
2021-11-23 16:03:30 +0700

    [#15] Add create method in KeywordsController

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -1,5 +1,24 @@
 # frozen_string_literal: true
+require 'csv'
 
 class KeywordsController < ApplicationController
   def index; end
+
+  def create
+    keywords = load_keywords
+    print(keywords)
+    redirect_to keywords_path
+  end
+
+  private
+
+  def load_keywords
+    keywords_file = params[:keywords_file]
+    csv_data = CSV.read(keywords_file.path)
+    keywords = []
+    csv_data.each do |row|
+      keywords.append(row.first)
+    end
+    keywords
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :keywords, only: [:index]
+  resources :keywords, only: [:index, :create]
 end
