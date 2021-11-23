5dbd9668ea867c52716a75cbd1203d2b5a0a67e2
Su Ho <su.v@nimblehq.co>
2021-11-23 16:23:17 +0700

    [#14] Update rubocop

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
 class KeywordsController < ApplicationController
-  def index
-  end
+  def index; end
 end
--- spec/requests/keywords_request_spec.rb
+++ spec/requests/keywords_request_spec.rb
@@ -1,12 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Keywords", type: :request do
-
-  describe "GET /index" do
-    it "returns http success" do
-      get "/keywords/index"
+RSpec.describe 'Keywords', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/keywords/'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
