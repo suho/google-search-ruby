2a8afc97d2c69819a5744fe6fff5c52036cb45c9
Su Ho <su.v@nimblehq.co>
2021-11-30 11:28:47 +0700

    [#15] Add tests for keywords controller (request type)

--- spec/requests/keywords_controller_spec.rb
+++ spec/requests/keywords_controller_spec.rb
@@ -11,4 +11,13 @@ RSpec.describe KeywordsController, type: :request do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe 'POST #create' do
+    it 'redirects to keywords index' do
+      sign_in Fabricate(:user)
+      post :create, params: keywords_file_params('keywords_valid.csv')
+
+      expect(response).to redirect_to keywords_path
+    end
+  end
 end
