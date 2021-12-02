7a583d55601374dba59acbbfd485b8e2f0f8ef45
Su Ho <su.v@nimblehq.co>
2021-12-02 22:07:49 +0700

    [#18] Update logic for save keywords

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -3,10 +3,15 @@
 require 'csv'
 
 class KeywordsController < ApplicationController
-  def index; end
+  def index
+    @keywords = current_user.keywords
+  end
 
   def create
     parse_keywords
+    parse_keywords.each do |key|
+      current_user.keywords.create(keyword: key)
+    end
     redirect_to keywords_path
   end
 
