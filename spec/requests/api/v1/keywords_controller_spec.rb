9982c0fe4be3d0b017b7dac3027c6486feb37a4b
Su Ho <su.v@nimblehq.co>
2022-01-21 18:12:25 +0700

    [#16] Add spec for response body testing

--- spec/requests/api/v1/keywords_controller_spec.rb
+++ spec/requests/api/v1/keywords_controller_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe API::V1::KeywordsController, type: :request do
   describe 'POST#create', api_authentication: :user do
     context 'when upload a valid file' do
       it 'returns created status' do
-        sign_in Fabricate(:user)
         post :create, params: keywords_file_params('valid_keywords.csv')
 
         expect(response).to have_http_status(:created)
@@ -15,10 +14,16 @@ RSpec.describe API::V1::KeywordsController, type: :request do
 
     context 'when upload no valid file' do
       it 'responds with alert message' do
-        sign_in Fabricate(:user)
         post :create
 
         expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it 'returns error response with invalid message', api_authentication: :user do
+        post :create
+        expected_response = { errors: [{ detail: 'Invalid file!', code: 'invalid_file' }] }
+
+        expect(JSON.parse(response.body, symbolize_names: true)).to eq(expected_response)
       end
     end
   end
