8247d64179f5153b0776df8bb73318730ee44944
Su Ho <su.v@nimblehq.co>
2022-01-21 16:54:20 +0700

    [#10] Add spec for happy path

--- spec/controllers/api/v1/tokens_controller_spec.rb
+++ spec/controllers/api/v1/tokens_controller_spec.rb
@@ -4,6 +4,16 @@ require 'rails_helper'
 
 RSpec.describe API::V1::TokensController, type: :controller do
   describe 'POST#revoke' do
+    context 'given a valid oauth application' do
+      it 'returns success status' do
+        application = Fabricate(:doorkeeper_application)
+        access_token = Fabricate(:access_token, application_id: application.id)
+        post :revoke, params: { token: access_token.token, client_id: application.uid, client_secret: application.secret }
+
+        expect(response).to have_http_status(:success)
+      end
+    end
+
     context 'given an invalid oauth application' do
       it 'returns an error message' do
         access_token = Fabricate(:access_token, application_id: Fabricate(:doorkeeper_application).id)
