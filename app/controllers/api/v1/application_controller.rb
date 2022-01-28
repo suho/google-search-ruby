e1f75ae5a010a80296cdd74e6f995792662fc084
Su Ho <su.v@nimblehq.co>
2022-01-28 18:52:06 +0700

    [#22] Update rescue_from with list of error

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -4,8 +4,7 @@ module API
   module V1
     class ApplicationController < ActionController::API
       include API::V1::DoorkeeperAuthentication
-      rescue_from ActiveRecord::RecordNotFound, with: :show_not_found_error
-      rescue_from Pagy::OverflowError, with: :show_not_found_error
+      rescue_from ActiveRecord::RecordNotFound, Pagy::OverflowError, with: :show_not_found_error
 
       private
 
